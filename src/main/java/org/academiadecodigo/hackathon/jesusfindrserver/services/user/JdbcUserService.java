3f5cc77832f0b8ed41ef178a8a6541f9f6a13631
Fábio Carvalho <fabcarvalho27@gmail.com>
2017-11-24 04:05:46 +0000

    added utils to check connection and close statement. authenticate and addUser

--- src/main/java/org/academiadecodigo/hackathon/jesusfindrserver/services/user/JdbcUserService.java
+++ src/main/java/org/academiadecodigo/hackathon/jesusfindrserver/services/user/JdbcUserService.java
@@ -4,11 +4,15 @@ import org.academiadecodigo.hackathon.jesusfindrserver.model.User;
 import org.academiadecodigo.hackathon.jesusfindrserver.persistence.DatabaseConnector;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 
 public class JdbcUserService implements UserService {
 
     private Connection dbConnection;
+    private Statement statement = null;
 
     public JdbcUserService() {
         dbConnection = DatabaseConnector.getInstance().getConnection();
@@ -16,12 +20,46 @@ public class JdbcUserService implements UserService {
 
     @Override
     public boolean authenticate(String username, String password) {
-        return false;
+
+        User user = findByName(username);
+        checkConnection();
+
+        if(user.getUsername() == null){
+            return false;
+        }
+
+        closeStatement();
+        return user.getPassword().equals(password);
     }
 
     @Override
     public void addUser(User user) {
 
+        checkConnection();
+
+        if(existingUser(user)){
+            return;
+        }
+
+        String query = "INSERT INTO users(username, password)VALUES (?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = dbConnection.prepareStatement(query);
+
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        closeStatement();
+    }
+
+    private boolean existingUser(User user) {
+        return findByName(user.getUsername()) != null;
     }
 
     @Override
@@ -32,5 +70,33 @@ public class JdbcUserService implements UserService {
     @Override
     public Collection<User> getUserList() {
         return null;
+    }
+
+    //utils
+    private void checkConnection(){
+        if(!isConnected()){
+            DatabaseConnector.getInstance().getConnection();
+        }
+    }
+
+    private boolean isConnected(){
+
+        boolean isConnected = false;
+        try {
+            isConnected = dbConnection != null && !dbConnection.isClosed();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isConnected;
+    }
+
+    private void closeStatement(){
+        if(statement != null){
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
     }
 }
